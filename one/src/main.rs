bfa0b9e05dd8bdc86613fc720e6b569256da65e0
Jeff <chessRPG@hotmail.com>
2024-11-30 21:47:15 -0800

    day 1 part 2

--- one/src/main.rs
+++ one/src/main.rs
@@ -20,8 +20,27 @@ fn part_one(input: String) -> u64 {
 }
 
 #[part_two]
-fn part_two(_: String) -> &'static str {
-    "incomplete"
+fn part_two(input: String) -> u64 {
+    let vals: Vec<_> = input.lines().into_iter().map(|line| line.split_once("   ").unwrap()).collect();
+    let mut lefts: Vec<u64> = Vec::new();
+    let mut rights: Vec<u64> = Vec::new();
+    for (l, r) in vals {
+        lefts.push(l.parse().unwrap());
+        rights.push(r.parse().unwrap());
+    }
+    let mut total_score = 0;
+
+    let mut counts = HashMap::new();
+    for val in rights {
+        counts.entry(val).and_modify(|c| *c += 1).or_insert(1);
+    }
+
+    for val in lefts {
+        total_score += val * *counts.entry(val).or_default();
+    }
+
+
+    total_score
 }
 
 harness!();
