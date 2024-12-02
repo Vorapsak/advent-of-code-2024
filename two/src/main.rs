d1dc123835d15fe3b97b8285d98e5287cedfc58d
Jeff <chessRPG@hotmail.com>
2024-12-02 14:33:36 -0800

    refactored part 1

--- two/src/main.rs
+++ two/src/main.rs
@@ -8,24 +8,26 @@ struct InputLine {
 
 #[part_one]
 fn part_one(input: List<InputLine, TermWith<NewLine>>) -> usize {
-    input.into_iter().map(|line| is_valid(line)).filter(|v| *v).count()
+    input.into_iter().filter(|line| is_valid(&line.nums)).count()
 }
 
-fn is_valid(line: InputLine) -> bool {
-    let mut current = &line.nums[0];
-    let mut dir = 1;
-    if line.nums[0] > line.nums[1] { dir = -1;}
-    for val in &line.nums[1..]{
+fn is_valid(line: &List<i32, SepBy<Space>>) -> bool {
+    let mut current = &line[0];
+    let descending = line[0] > line[1];
+    for val in &line[1..]{
         if val.abs_diff(*current) > 3 || val == current {return false}
-        if (dir > 0 && current > val) || (dir < 0 && current < val) {return false}
+        if (!descending && current > val) || (descending && current < val) {return false}
         current = val;
     }
     true
 }
 
 #[part_two]
-fn part_two(_: String) -> &'static str {
-    "incomplete"
+fn part_two(input: List<InputLine, TermWith<NewLine>>) -> usize {
+    for line in input {
+        
+    }
+    0
 }
 
-harness!();
+harness!(part_1: 218);
