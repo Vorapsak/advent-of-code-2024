bb0aa748d3412578cfe1a3b816f311ccd4101f47
Jeff <chessRPG@hotmail.com>
2024-12-08 19:52:29 -0800

    day 5 part 2

--- five/src/main.rs
+++ five/src/main.rs
@@ -48,14 +48,48 @@ fn part_one(input: String) -> u32 {
     let books: Vec<Book> = books.lines().map(|line| Book::from(line)).collect();
     
     books.iter()
-    .filter(|&book| book.satisfies(&rules))
-    .map(|book| book.pages[book.pages.len()/2])
-    .sum()
+        .filter(|&book| book.satisfies(&rules))
+        .map(|book| book.pages[book.pages.len()/2])
+        .sum()
 }
+
+
+fn fixup(book: &Book, rules: &Vec<Rule>) -> Book {
+    let mut pages = book.pages.clone();
+    rules.iter().for_each(|rule|
+        match (pages.iter().position(|&x| x == rule.first), pages.iter().position(|&x| x == rule.second)) {
+            (Some(a), Some(b)) => if a > b { pages.swap(a, b)},
+            (None, _) => (),
+            (_, None) => (),
+        }
+    );
+    
+    Book {
+        pages: pages
+    }
+}
+
 
 #[part_two]
-fn part_two(_: String) -> &'static str {
-    "incomplete"
+fn part_two(input: String) -> u32 {
+
+    let (rules, books) = input.split_once("\n\n").unwrap();
+    let rules: Vec<Rule> = rules.lines().map(|line| Rule::from(line)).collect();
+    let books: Vec<Book> = books.lines().map(|line| Book::from(line)).collect();
+    
+    let mut fixed: Vec<Book> = books.iter()
+        .filter(|&book| !book.satisfies(&rules))
+        .map(|book| fixup(&book, &rules)).collect();
+
+    while fixed.iter().filter(|&book| book.satisfies(&rules)).count() != fixed.len() {
+            fixed = fixed.iter().map(|book| fixup(&book, &rules)).collect();
+    }
+
+
+    fixed.iter()
+        .map(|book| book.pages[book.pages.len()/2])
+        .sum()
+
 }
 
-harness!(part_1: 5588);
+harness!(part_1: 5588, part_2: 5331);
