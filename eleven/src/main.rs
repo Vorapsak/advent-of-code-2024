06a99edfb4a9f97ef838c9833442d467ce780911
Jeff <chessRPG@hotmail.com>
2024-12-11 11:28:02 -0800

    day 11 part 2

--- eleven/src/main.rs
+++ eleven/src/main.rs
@@ -2,45 +2,45 @@ use advent::prelude::*;
 
 #[part_one]
 fn part_one(input: List<usize, SepBy<Space>>) -> usize {
-    let mut current_stones: Vec<usize> = input.into_iter().collect();
+    let mut current_stones: HashMap<usize, usize> = input.into_iter().map(|val| (val, 1)).collect();
 
     for _ in 0..25 {
         current_stones = evolve(current_stones);
     }
-
-    current_stones.len()
+    current_stones.values().sum()
 }
 
-fn evolve(stones: Vec<usize>) -> Vec<usize> {
-    let mut next = Vec::new();
+fn evolve(stones: HashMap<usize, usize>) -> HashMap<usize, usize> {
+    let mut next = HashMap::new();
 
-    for stone in stones {
-        if stone == 0 {
-            next.push(1);
-        } else if stone.to_string().len() % 2 == 0 {
-            let stonestr = stone.to_string();
+    for (label, count) in stones {
+        if (label) == 0 {
+            *next.entry(1).or_default() += count;
+        } else if label.to_string().len() % 2 == 0 {
+            let stonestr = label.to_string();
             let (p1, p2) = stonestr.split_at(stonestr.len()/2);
-            next.push(p1.parse().unwrap());
-            next.push(p2.parse().unwrap());
+            let p1 = p1.parse().unwrap();
+            let p2 = p2.parse().unwrap();
+            *next.entry(p1).or_default() += count;
+            *next.entry(p2).or_default() += count;
         } else {
-            next.push(stone * 2024);
+            *next.entry(label * 2024).or_default() += count;
         }
     }
-
 
     next
 }
 
 #[part_two]
 fn part_two(input: List<usize, SepBy<Space>>) -> usize {
-    let mut current_stones: Vec<usize> = input.into_iter().collect();
+    let mut current_stones: HashMap<usize, usize> = input.into_iter().map(|val| (val, 1)).collect();
 
-    for _ in 0..25 {
+    for _ in 0..75 {
         current_stones = evolve(current_stones);
     }
 
-    current_stones.len()
+    current_stones.values().sum()
 
 }
 
-harness!(part_1: 203609);
+harness!(part_1: 203609, part_2: 240954878211138);
