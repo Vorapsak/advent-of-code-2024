2c2428d7f0a9ef9488cba2242bc3e11df426ef39
Jeff <chessRPG@hotmail.com>
2024-12-18 22:42:58 -0800

    day 19 part 1

---
+++ nineteen/src/main.rs
@@ -0,0 +1,28 @@
+use advent::prelude::*;
+use regex::Regex;
+
+#[part_one]
+fn part_one(input: String) -> usize {
+    let towels: String = input.lines().take(1).collect();
+    let targets: Vec<&str> = input.lines().skip(2).collect();
+    let mut satisfiable = 0;
+    let towels = towels.split(", ").collect::<Vec<_>>();
+
+    let re = Regex::new(&("^(".to_owned() + &towels.join("|") + ")+$")).unwrap();
+
+     for pattern in targets {
+        //if try_parsing(&towels, &pattern) {
+        if re.is_match(pattern) {
+            satisfiable += 1;
+        }
+    } 
+
+    satisfiable
+}
+
+#[part_two]
+fn part_two(_: String) -> &'static str {
+    "incomplete"
+}
+
+harness!(part_1: 300);
