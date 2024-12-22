30c9539dcba25cc351e199718ca2ab43656ba8c6
Jeff <chessRPG@hotmail.com>
2024-12-21 22:30:48 -0800

    day 22 part 2

--- twenty-two/src/main.rs
+++ twenty-two/src/main.rs
@@ -2,7 +2,6 @@ use advent::prelude::*;
 
 #[part_one]
 fn part_one(input: List<u64, TermWith<NewLine>>) -> u64 {
-    dbg!(&input);
     let mut total = 0;
     for seed in input {
         let mut secret = seed;
@@ -24,8 +23,95 @@ fn part_one(input: List<u64, TermWith<NewLine>>) -> u64 {
 }
 
 #[part_two]
-fn part_two(_: String) -> &'static str {
-    "incomplete"
+fn part_two(input: List<i64, TermWith<NewLine>>) -> i64 {
+    let mut sequences: HashMap<(i64, i64, i64, i64), usize> = HashMap::new();
+
+    for &seed in input.iter() {
+        let mut seed_seqs: HashSet<(i64, i64, i64, i64)> = HashSet::new();
+        let mut secret = seed;
+        let t = iterate(secret);
+        let mut four_ago = (t%10) - (secret%10);
+        secret = t;
+        let t = iterate(secret);
+        let mut three_ago = (t%10) - (secret%10);
+        secret = t;
+        let t = iterate(secret);
+        let mut two_ago = (t%10) - (secret%10);
+        secret = t;
+        let t = iterate(secret);
+        let mut one_ago = (t%10) - (secret%10);
+        secret = t;
+        sequences.insert((four_ago, three_ago, two_ago, one_ago), 1);
+        for _ in 4..2000 {
+            let next = iterate(secret);
+            four_ago = three_ago;
+            three_ago = two_ago;
+            two_ago = one_ago;
+            one_ago = (next%10) - (secret%10);
+            secret = next;
+            seed_seqs.insert((four_ago, three_ago, two_ago, one_ago));
+        }
+
+        for seq in seed_seqs {
+            *sequences.entry(seq).or_default() += 1;
+        }
+    }
+
+    let mut max_bananas = 0;
+
+    for (sequence, count) in sequences {
+        if count < 370 { continue;}
+        let next = calculate_profits(sequence, &input);
+        if next > max_bananas {
+            max_bananas = next;
+        }
+    }
+
+    max_bananas
 }
 
-harness!(part_1: 20506453102);
+fn calculate_profits (sequence: (i64, i64, i64, i64), input: &List<i64, TermWith<NewLine>>) -> i64 {
+    let mut profit = 0;
+    for &seed in input {
+        
+        let mut secret = seed;
+        let t = iterate(secret);
+        let mut four_ago = (t%10) - (secret%10);
+        secret = t;
+        let t = iterate(secret);
+        let mut three_ago = (t%10) - (secret%10);
+        secret = t;
+        let t = iterate(secret);
+        let mut two_ago = (t%10) - (secret%10);
+        secret = t;
+        let t = iterate(secret);
+        let mut one_ago = (t%10) - (secret%10);
+        secret = t;
+
+        for _ in 4..2000 {
+            if (four_ago, three_ago, two_ago, one_ago) == sequence {profit += secret%10; break;}
+            let next = iterate(secret);
+            four_ago = three_ago;
+            three_ago = two_ago;
+            two_ago = one_ago;
+            one_ago = (next%10) - (secret%10);
+            secret = next;
+        }
+    }
+    profit
+}
+
+fn iterate(mut secret: i64) -> i64 {
+    let t = secret * 64;
+    secret = secret ^ t;
+    secret %= 16777216;
+    let t = secret / 32;
+    secret = secret ^ t;
+    secret %= 16777216;
+    let t = secret * 2048;
+    secret = secret ^ t;
+    secret %= 16777216;
+    secret
+}
+
+harness!(part_1: 20506453102, part_2: 2423);
